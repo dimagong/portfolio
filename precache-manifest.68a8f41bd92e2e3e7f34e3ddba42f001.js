83fe0cb383f7a20797a628d5aa363e37dce61761
dimagong <dimagong@gmail.com>
2020-07-09 19:45:39 +0300

    Updates

---
+++ precache-manifest.68a8f41bd92e2e3e7f34e3ddba42f001.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0c997e0085333c255d02b0be910151",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9b1d2579ea438d391e75",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc643b31c6fba5dc6852",
+    "url": "/portfolio/static/js/2.6175625b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.6175625b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1d2579ea438d391e75",
+    "url": "/portfolio/static/js/main.546bd9b6.chunk.js"
+  },
+  {
+    "revision": "4098d9d28d8120ca29b4",
+    "url": "/portfolio/static/js/runtime-main.6a994c90.js"
+  },
+  {
+    "revision": "e2446a544c0f81548c729bfcf7afb02b",
+    "url": "/portfolio/static/media/Layout.e2446a54.png"
+  },
+  {
+    "revision": "830cac33af5bb0d7aa782f4430f59779",
+    "url": "/portfolio/static/media/PopularMovie.830cac33.png"
+  },
+  {
+    "revision": "c0cb1730d5f1249a5f3e9e7adfe7274d",
+    "url": "/portfolio/static/media/chacknoris_4.c0cb1730.png"
+  },
+  {
+    "revision": "07402ffcd9c5ac22acc82ee037de32b6",
+    "url": "/portfolio/static/media/firebase.07402ffc.png"
+  },
+  {
+    "revision": "fbffb25e78ab33b472df4b3cc734b3ad",
+    "url": "/portfolio/static/media/map3D.fbffb25e.png"
+  },
+  {
+    "revision": "02e3d21a08e33725c57dd9037b6ce276",
+    "url": "/portfolio/static/media/movie1.02e3d21a.png"
+  },
+  {
+    "revision": "1b149940f9c9e7f35de11df79ae47db0",
+    "url": "/portfolio/static/media/session.1b149940.png"
+  },
+  {
+    "revision": "5c3756f15b25985dcb2a80cb400c67d8",
+    "url": "/portfolio/static/media/shop.5c3756f1.png"
+  }
+]);
