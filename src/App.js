652478f04caeda70af5372280e8089c18cc125fe
dimagong <dimagong@gmail.com>
2020-07-10 07:45:13 +0300

    changed url

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-      http://dimagong.site/
+      dimagong.github.io/portfolio
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
